f67f41cfbb657cd679ee16c3a02d3a475e8b42fb
backroomdev <justin@thebackroom.app>
2022-07-23 06:59:22 -0400

    Initial checkin

---
+++ Source/MSBuild.Community.Tasks/Git/GitConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MSBuild.Community.Tasks.Git
+{
+    public static class GitConstants
+    {
+        public const string Git = "git";
+        public const string GetUrl = "get-url";
+        public const string Origin = "origin";
+    }
+}
---
+++ Source/MSBuild.Community.Tasks/Git/GitGetRepositoryUrl.cs
@@ -0,0 +1,34 @@
+﻿//
+//  GitGetRepositoryUrl.cs
+//
+//  Authors:
+//       David G. Moore, Jr. <david@dgmjr.io>
+//
+//  Copyright ©️ 2022 David G. Moore, Jr.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+namespace MSBuild.Community.Tasks.Git;
+
+using static GitConstants;
+
+public class GitGetRepositoryUrl : GitClient
+{
+    public GitGetRepositoryUrl()
+    {
+        Command = GetUrl;
+        Arguments = Origin;
+    }
+}
